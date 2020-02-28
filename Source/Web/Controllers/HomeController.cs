190a878544f279f4f1f4e00bc429889dbff3d51b
taulfsime <34596275+taulfsime@users.noreply.github.com>
2020-02-28 20:14:01 +0200

    Fixed login page and added validation for user login

--- Source/Web/Controllers/HomeController.cs
+++ Source/Web/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
+using Data;
+using Data.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Web.Models.Home;
 
@@ -9,15 +13,60 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly MyHotelDb _context;
+
+        public HomeController()
         {
-            return View();
+            _context = new MyHotelDb();
         }
 
-        public async Task<IActionResult> Login(HomeIndexViewModel model)
+        //GET: /Index
+        public IActionResult Index()
         {
-            //Login 
+            LoginViewModel model = new LoginViewModel();
+
             return View(model);
+        }
+
+        //POST: /Index
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            User user = _context.Users.ToArray().Where(u => u.Username == model.Username).FirstOrDefault();
+            if(user != null)
+            {
+                if(user.PasswordHash == GetPasswordHash(model.PasswordHash))
+                {
+                    return Redirect("/Users");
+                }
+                else
+                {
+                    ModelState.AddModelError("PasswordHash", "Password does not match!");
+                    return View(model);
+                }
+            }
+
+            ModelState.AddModelError("Username", "User with that username does not exist!");
+            return View(model);
+        }
+
+        private string GetPasswordHash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
